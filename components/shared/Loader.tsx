cc2d491b4f615bf2f4c5f15a65bda4d23a69dfa2
Ahmed <ahmed15ayman7ahmed2002@gmail.com>
2024-07-24 15:42:32 +0300

    update dashboard

--- components/shared/Loader.tsx
+++ components/shared/Loader.tsx
@@ -1,16 +1,19 @@
-const Loader = ({is}:{is?:boolean}) => (
-  <div className="fixed left-1/2 top-10 translate-x-1/2">
 
-    <div className={`${is?'flex justify-center items-center h-[20vh] ':''} w-full`}>
+const Loader = ({ is }: { is?: boolean }) => (
+  <div className="fixed left-1/2 top-10 -translate-x-1/2">
+    <div
+      className={`${
+        is ? "flex justify-center items-center h-[20vh] " : ""
+      } w-full`}>
       <img
         src={`/assets/loader.svg`}
         alt="loader"
-        width={is?100:24}
-        height={is?100:24}
+        width={is ? 100 : 24}
+        height={is ? 100 : 24}
         className={`animate-spin`}
-        />
+      />
     </div>
-        </div>
-  );
-  
-  export default Loader;
+  </div>
+);
+
+export default Loader;
